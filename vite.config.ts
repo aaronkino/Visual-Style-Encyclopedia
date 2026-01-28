3dba677496d254da8fbf4933913865c4fe58cfa3
Aaron KINO <aaron18610@gmail.com>
2026-01-28 14:24:28 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Crucial for GitHub Pages to load assets correctly
+  base: './', // Crucial for GitHub Pages
   build: {
     outDir: 'dist',
+  },
+  define: {
+    // This prevents "Uncaught ReferenceError: process is not defined" in the browser
+    'process.env': {} 
   }
 });
